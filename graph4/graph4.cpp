61159e2225efd324362f7721ad50738bfed68876
博琨 张 <428749119@qq.com>
2026-05-11 18:25:45 +0700

    修改输入方式为文件

--- graph4/graph4.cpp
+++ graph4/graph4.cpp
@@ -1,38 +1,34 @@
 ﻿#include <iostream>
 #include <cstdio>
+#include <fstream>
+#include <string>
+
 using namespace std;
+const int n = 8;
 
 int main() {
-    int L[22][22], i, j, n;
-    char S[21];  // 存储一行迷宫字符串
-
-    cin >> n;
-    cin.ignore(); // 吸收换行符，避免读取空行
-
-    for (i = 1; i <= n; i++) {
-        fgets(S, 21, stdin);
+    int L[22][22] = {0}, i, j;
+    ifstream fin("input.txt");
+    for (i = 1; i <= n;i++) {
         for (j = 1; j <= n; j++) {
-            if (S[j - 1] == '-') {
-                L[i][j] = 0;
-            }
-            if (S[j - 1] == '#') {
-                L[i][j] = 1000;
-            }
+            fin >> L[i][j];
         }
     }
+    fin.close();
+
     for (j = 0; j <= n + 1; j++) {
-        L[0][j] = 1000;
-        L[n + 1][j] = 1000;
+        L[0][j] = -1;
+        L[n + 1][j] = -1;
     }
     for (i = 1; i <= n; i++) {
-        L[i][0] = 1000;
-        L[i][n + 1] = 1000;
+        L[i][0] = -1;
+        L[i][n + 1] = -1;
     }
     int si, sj, fi, fj;
     cin >> si >> sj;
     cin >> fi >> fj;
 
-    if (L[si][sj] == 1000 || L[fi][fj] == 1000) {
+    if (L[si][sj] == -1 || L[fi][fj] == -1) {
         cout << "Cannot reach the end point" << endl;
         return 0;
     }
@@ -110,14 +106,14 @@ int main() {
     while (k > 0)
     {
         Mi[k] = i; Mj[k] = j;  // 记录当前点
-        if (L[i - 1][j] < L[i][j]) i--;
-        else if (L[i][j - 1] < L[i][j]) j--;
-        else if (L[i + 1][j] < L[i][j]) i++;
-        else if (L[i][j + 1] < L[i][j]) j++;
-        else if (L[i - 1][j - 1] < L[i][j]) { i--; j--; }
-        else if (L[i - 1][j + 1] < L[i][j]) { i--; j++; }
-        else if (L[i + 1][j - 1] < L[i][j]) { i++; j--; }
-        else if (L[i + 1][j + 1] < L[i][j]) { i++; j++; }
+        if (L[i - 1][j] > 0 && L[i - 1][j] < L[i][j]) i--;
+        else if (L[i][j - 1] > 0 && L[i][j - 1] < L[i][j]) j--;
+        else if (L[i + 1][j] > 0 && L[i + 1][j] < L[i][j]) i++;
+        else if (L[i][j + 1] > 0 && L[i][j + 1] < L[i][j]) j++;
+        else if (L[i - 1][j - 1] > 0 && L[i - 1][j - 1] < L[i][j]) { i--; j--; }
+        else if (L[i - 1][j + 1] > 0 && L[i - 1][j + 1] < L[i][j]) { i--; j++; }
+        else if (L[i + 1][j - 1] > 0 && L[i + 1][j - 1] < L[i][j]) { i++; j--; }
+        else if (L[i + 1][j + 1] > 0 && L[i + 1][j + 1] < L[i][j]) { i++; j++; }
         k--;
     }
     for (int a = 1; a <= L[fi][fj]; a++) {
@@ -125,7 +121,7 @@ int main() {
     }
     for (i = 1; i <= n; i++) {
         for (j = 1; j <= n; j++) {
-            if (L[i][j] == 1000) {
+            if (L[i][j] == -1) {
                 cout << '#';
             }
             else if (path[i][j]) {
